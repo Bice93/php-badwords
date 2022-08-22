999c11e5c2f490d4b6f7119944bb3e4c01635df2
Bice93 <bianca.cerulo@hotmail.it>
2022-08-22 15:26:49 +0200

    Censored Word

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+    // Creare una variabile con un paragrafo di testo a vostra scelta.
+    $paragraphText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquid, natus vitae quo enim totam corrupti, laudantium voluptatem nihil, quam blanditiis tempore aspernatur! Incidunt labore recusandae, quas iusto vitae iste. Lorem dolor!';
+    
+    $userWord = $_GET['word'];
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +16,28 @@
 </head>
 <body>
     <h1>PHP BADWORDS</h1>
+    <p>
+    
+        <?php
+            // Stampo a schermo il paragrafo e la sua lunghezza.
+            echo $paragraphText;
+            echo strlen($paragraphText);
+        ?>
+    </p>
+    <h5>Parola ricercata: 
+        <?php 
+        // Stampo a schermo la parola ricercata
+            echo $userWord;
+        ?>
+    </h5>
+    <p>
+        <?php 
+        // Sostituisco con *** la parola da censurare 
+        $wordCensored = str_replace($userWord, '***', $paragraphText);
+        // Stampo a schermo il nuovo paragrafo con la parola censurata e la lunghezza
+        echo $wordCensored;
+        echo strlen($wordCensored);
+        ?>
+    </p>
 </body>
 </html>
